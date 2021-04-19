f6c09afcd6fb78be87c632c54ea4f602d3ee5a3d
Arif <itarif.diit@gmail.com>
2021-04-19 23:33:37 +0600

    Header and sideber menu has been completed

--- resources/views/partials/_sideber.blade.php
+++ resources/views/partials/_sideber.blade.php
@@ -1,63 +1,36 @@
 <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-      <div class="position-sticky pt-3">
-        <ul class="list-unstyled ps-0">
-          <li class="mb-1">
-            <div class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#dashboard-collapse" aria-expanded="false">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
-              Guest
-            </div>
-            <div class="collapse" id="dashboard-collapse" style="">
-              <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                <li><a class="dropdown-item" href="{{route('all.guest')}}">All Guest</a></li>
-              </ul>
-            </div>
-          </li>
-          <li class="mb-1">
-            <div class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#orders-collapse" aria-expanded="false">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
-              Host
-            </div>
-            <div class="collapse" id="orders-collapse" style="">
-              <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                <li><a class="dropdown-item" href="{{route('all.host')}}">All Host</a></li>
-              </ul>
-            </div>
-          </li>
-          <li class="mb-1">
-            <div class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#orders-collapse" aria-expanded="false">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
-              Balance
-            </div>
-            <div class="collapse" id="orders-collapse" style="">
-              <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                <li><a class="dropdown-item" href="{{route('all.balance')}}">All Balance</a></li>
-              </ul>
-            </div>
-          </li>
-          <li class="mb-1">
-            <div class="btn btn-toggle align-items-center rounded collapsed" data-bs-toggle="collapse" data-bs-target="#orders-collapse" aria-expanded="false">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
-              Report
-            </div>
-            <div class="collapse" id="orders-collapse" style="">
-              <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                <li><a class="dropdown-item" href="#">Approve Of Settlement Request</a></li>
-                <li><a class="dropdown-item" href="#">Date Wise Host Report</a></li>
-                <li><a class="dropdown-item" href="#">Month Wise Host Report</a></li>
-                <li><a class="dropdown-item" href="#">Call Details</a></li>
-              </ul>
-            </div>
-          </li>
-        </ul>
-        {{-- <div class="dropdown">
-          <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown link
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+  <aside class="bd-aside sticky-xl-top text-muted align-self-start mb-3 mb-xl-5 px-2">
+    {{-- <h2 class="h6 pt-4 pb-3 mb-4 border-bottom">On this page</h2> --}}
+    <nav class="small" id="toc">
+      <ul class="list-unstyled">
+        <li class="my-2">
+          <button class="btn d-inline-flex align-items-center collapsed" data-bs-toggle="collapse" aria-expanded="false" data-bs-target="#contents-collapse" aria-controls="contents-collapse">Guest</button>
+          <ul class="list-unstyled ps-3 collapse" id="contents-collapse">
+            <li><a class="dropdown-item" href="{{route('all.guest')}}">All Guest</a></li>
           </ul>
-        </div> --}}
-      </div>
+        </li>
+        <li class="my-2">
+          <button class="btn d-inline-flex align-items-center collapsed" data-bs-toggle="collapse" aria-expanded="false" data-bs-target="#forms-collapse" aria-controls="forms-collapse">Host</button>
+          <ul class="list-unstyled ps-3 collapse" id="forms-collapse" style="">
+            <li><a class="dropdown-item" href="{{route('all.host')}}">All Host</a></li>
+          </ul>
+        </li>
+        <li class="my-2">
+          <button class="btn d-inline-flex align-items-center collapsed" data-bs-toggle="collapse" aria-expanded="false" data-bs-target="#components-collapse" aria-controls="components-collapse">Balance</button>
+          <ul class="list-unstyled ps-3 collapse" id="components-collapse">
+            <li><a class="dropdown-item" href="{{route('all.balance')}}">All Balance</a></li>
+          </ul>
+        </li>
+        <li class="my-2">
+          <button class="btn d-inline-flex align-items-center collapsed" data-bs-toggle="collapse" aria-expanded="false" data-bs-target="#components-collapse" aria-controls="components-collapse">Report</button>
+          <ul class="list-unstyled ps-3 collapse" id="components-collapse">
+            <li><a class="dropdown-item" href="#">Approve Of Settlement Request</a></li>
+            <li><a class="dropdown-item" href="#">Date Wise Host Report</a></li>
+            <li><a class="dropdown-item" href="#">Month Wise Host Report</a></li>
+            <li><a class="dropdown-item" href="#">Call Details</a></li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+  </aside>
     </nav>
